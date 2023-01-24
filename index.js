d3c343452039aae138245733e503b736a8e20cf7
Naufal Risnawa <naufalfakhri2174@gmail.com>
2023-01-24 20:17:22 +0700

    clean code

--- index.js
+++ index.js
@@ -1,62 +1,42 @@
-const express = require("express") // memanggil library express js
-const bodyParser = require("body-parser") // memanggil library bodyparser
-const cors = require("cors") // memanggil library cors
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
 const app = express()
-// penggunaan body-parser untuk ekstrak data request berformat JSON
+
 app.use(bodyParser.json())
-// penggunaan body-parser untuk ekstrak data request dari body
 app.use(bodyParser.urlencoded({extended: true}))
-// penggunaan cors agar end point dapat diakses oleh cross platform
 app.use(cors())
-// endpoint "/test" dengan method GET
+
 app.get("/test", (req,res) => {
-    // req merupakan variabel yang berisi data request
-    // res merupakan variabel yang berisi data response dari end-point
-    // membuat objek yang berisi data yang akan dijadikan response
     let response = {
-    message: "Ini end-point pertama ku",
-    method: req.method,
- code: res.statusCode
- }
- // memberikan response dengan format JSON yang berisi objek di atas
- res.json(response)
-})
-// menjalankan server pada port 8000
-app.listen(8000, () => {
-    console.log("Server run on port 8000");
-})
-// endpoint "/profil/nama/umur" dengan method GET
-app.get("/profil/:name/:age", (req,res) => {
-    // :name dan :age → diberikan titik dua didepan menunjukkan "name"dan "age"
-    // bersifat dinamis yang dapat diganti nilai nya saat melakukan request
-    // menampung data yang dikirimkan
-let name = req.params.name // mengambil nilai pada parameter name
-let age = req.params.age // mengambil nilai pada parameter age
-// membuat objek yang berisi data yang akan dijadikan response
-// response berisi data nama dan umur sesuai dengan nilai parameter
-let response = {
-nama: name,
-umur: age
-}
-// memberikan response dengan format JSON yang berisi objek di atas
-res.json(response)
+        message: "Ini end-point pertama ku",
+        method: req.method,
+        code: res.statusCode
+    }
+    res.json(response)
 })
 
-// end-point "/bujur_sangkar" dengan method POST
+app.get("/profil/:name/:age", (req,res) => {
+    let name = req.params.name
+    let age = req.params.age 
+    let response = {
+        nama: name,
+        umur: age
+    }
+    res.json(response)
+})
+
 app.post("/bujur_sangkar", (req,res) => {
-    // menampung data yang dikirimkan dan mengkonversi menjadi tipe numerik
-    let panjang = Number(req.body.panjang) // mengambil nilai panjang dari body
-    let lebar = Number(req.body.lebar) // mengamil nilai lebar dari body
+    let panjang = Number(req.body.panjang) 
+    let lebar = Number(req.body.lebar) 
     let luas = panjang * lebar
     let keliling = 2 * (panjang + lebar)
-    // membuat objek yang berisi data yang akan dijadikan response
     let response = {
-    panjang: panjang,
-    lebar: lebar,
-    luas: luas,
-    keliling: keliling
+        panjang: panjang,
+        lebar: lebar,
+        luas: luas,
+        keliling: keliling
     }
-    // memberikan response dengan format JSON yang berisi objek di atas
     res.json(response)
 })
 
@@ -68,11 +48,11 @@ app.post("/kubus", (req,res) => {
     let volume = sisi **3
     let luas_permukaan= 6 * sisi **2
     let response = {
-    sisi: sisi,
-    result: {
-        volume: volume,
-        luas_permukaan: luas_permukaan
-    }
+        sisi: sisi,
+        result: {
+            volume: volume,
+            luas_permukaan: luas_permukaan
+        }
     }
     res.json(response)
 })
@@ -83,13 +63,13 @@ app.post("/balok", (req,res) => {
     let volume = panjang * lebar * tinggi
     let luas_permukaan= 2 * (panjang*lebar + panjang*tinggi + tinggi*lebar)
     let response = {
-    panjang: panjang,
-    lebar: lebar,
-    tinggi: tinggi,
-    result: {
-        volume: volume,
-        luas_permukaan: luas_permukaan
-    }
+        panjang: panjang,
+        lebar: lebar,
+        tinggi: tinggi,
+        result: {
+            volume: volume,
+            luas_permukaan: luas_permukaan
+        }
     }
     res.json(response)
 })
@@ -391,3 +371,8 @@ app.post("/matrix", (req,res) => {
 res.json(response)
 })
 //                                      LATIHAN SOAL                                      // 
+
+
+app.listen(8000, () => {
+    console.log("Server run on port 8000");
+})
